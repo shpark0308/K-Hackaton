6276df71ce490cb33d6012956be280fc8526488f
shpark0308 <60208434+shpark0308@users.noreply.github.com>
2020-08-05 00:00:15 +0900

    shpark0308 2020/08/04 Final Version

--- app/src/main/java/com/example/we_in/camera/CameraCardDownFragment.java
+++ app/src/main/java/com/example/we_in/camera/CameraCardDownFragment.java
@@ -17,42 +17,70 @@ import androidx.fragment.app.Fragment;
 import com.example.we_in.R;
 
 public class CameraCardDownFragment extends Fragment { // Test
-    LinearLayout carddown1,cardup1;
+    LinearLayout carddown1,cardopen;
     ViewGroup viewGroup;
     Animation translateUp = null;
     Animation translateDown = null;
+    Animation CardUp = null;
+    Animation CardDown = null;
 
-    boolean isPageState = false;
+    boolean[] isPageState = new boolean[2];
+    int num;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_camera_card_down1,container,false);  /* -- 이거 fragment_camera_card_down 로 고치기 -- */
         carddown1 = (LinearLayout) viewGroup.findViewById(R.id.card_down);
-        cardup1 = (LinearLayout) viewGroup.findViewById(R.id.card_up);
+        cardopen = (LinearLayout) viewGroup.findViewById(R.id.first_open);
 
         translateDown = AnimationUtils.loadAnimation(this.getContext(),R.anim.translate_down);
         translateUp = AnimationUtils.loadAnimation(this.getContext(),R.anim.translate_up);
+        CardUp = AnimationUtils.loadAnimation(this.getContext(),R.anim.card_up);
+        CardDown = AnimationUtils.loadAnimation(this.getContext(),R.anim.card_down);
 
         SlidingAnimationListener listener = new SlidingAnimationListener();
         translateDown.setAnimationListener(listener);
         translateUp.setAnimationListener(listener);
+        CardUp.setAnimationListener(listener);
+        CardDown.setAnimationListener(listener);
+        isPageState[0] = false;
+        isPageState[1] = false;
+        num=1;
 
-        carddown1.setOnClickListener(new View.OnClickListener(){
+        /*carddown1.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                Log.d("첫번째", isPageState+"");
-                cardup1.startAnimation(translateUp);
-                carddown1.setVisibility(View.INVISIBLE);
+                Log.d("두번째", isPageState[0]+"//"+isPageState[1]);
+                carddown1.startAnimation(translateUp);
+                Log.d("이걸 걸치고 가나", isPageState[0]+"//"+isPageState[1]);
+                cardopen.startAnimation(CardUp);
             }
         });
-        cardup1.setOnClickListener(new View.OnClickListener() {
+        cardopen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("두번째", isPageState+"");
-                cardup1.startAnimation(translateDown);
-                cardup1.setVisibility(View.INVISIBLE);
+                Log.d("첫번째", isPageState[0]+"//"+isPageState[1]);
+                carddown1.startAnimation(translateDown);
+                cardopen.startAnimation(CardDown);
+            }
+        });*/
+        carddown1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPageState[0]&&isPageState[1]){
+                    Log.d("첫번째", isPageState[0]+"//"+isPageState[1]);
+                    carddown1.startAnimation(translateDown);
+                    cardopen.startAnimation(CardDown);
+                }
+                else{
+                    Log.d("두번째", isPageState[0]+"//"+isPageState[1]);
+                    carddown1.startAnimation(translateUp);
+                    Log.d("이걸 걸치고 가나", isPageState[0]+"//"+isPageState[1]);
+                    cardopen.startAnimation(CardUp);
+                }
             }
         });
 
@@ -70,15 +98,32 @@ public class CameraCardDownFragment extends Fragment { // Test
 
         @Override
         public void onAnimationEnd(Animation animation) {
-            if (isPageState){  // 안보이게 만드는 것
-                Log.d("세번째", isPageState+"");
-                carddown1.setVisibility(View.VISIBLE);
-                isPageState = false;
+            if (isPageState[1]){  // 안보이게 만드는 것
+                Log.d("세번째", isPageState[0]+"//"+isPageState[1]+""+num);
+                //carddown1.setVisibility(View.VISIBLE);
+                if (num==1){
+                    isPageState[0]=false;
+                    num++;
+                }
+                else {
+                    isPageState[1]=false;
+                    num=1;
+                }
+
             }
             else{
-                Log.d("네번째", isPageState+"");
-                cardup1.setVisibility(View.VISIBLE);
-                isPageState = true;
+                Log.d("네번째", isPageState[0]+"//"+isPageState[1]+""+num);
+                //cardup1.setVisibility(View.VISIBLE);
+                if (num==1){
+                    isPageState[0]=true;
+                    num++;
+                }
+                else{
+                    isPageState[1]=true;
+                    num=1;
+                }
+
+
             }
 
         }
